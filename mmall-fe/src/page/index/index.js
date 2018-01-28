8baca5d3a6e4acd9244c2aea6e782863cb24ad90
Songzhiguo <Fu_shide@163.com>
2018-01-28 14:34:35 +0800

    通用js工具类封装

--- mmall-fe/src/page/index/index.js
+++ mmall-fe/src/page/index/index.js
@@ -1,3 +1,7 @@
 'use strict';
-require('../module.js');
-require('./index.css');
+var _mm = require('util/mm.js');
+var html = '<div>{{data}}</div>';
+var data = {
+    data: 1
+};
+console.log(_mm.renderHtml(html, data));
