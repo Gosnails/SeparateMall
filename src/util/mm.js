1730652fe8b70a19e3493193c315ceb6c708e731
Songzhiguo <Fu_shide@163.com>
2018-02-02 18:56:49 +0800

    列表的排序及分页功能

--- src/util/mm.js
+++ src/util/mm.js
@@ -6,55 +6,55 @@ var conf = {
 };
 var _mm = {
     // 网络请求
-    request: function(param) {
+    request: function (param) {
         var _this = this;
         $.ajax({
             type: param.method || 'get',
             url: param.url || '',
             dataType: param.type || 'json',
             data: param.data || '',
-            success: function(res) {
+            success: function (res) {
                 //请求成功
                 if (0 === res.status) {
                     typeof param.success === 'function' && param.success(res.data, res.msg);
-                }else if(10 === res.status) { // 没有登录状态，需要强制登录
+                } else if (10 === res.status) { // 没有登录状态，需要强制登录
                     _this.doLogin();
-                }else if(1 === res.status) { // 没有数据错误
+                } else if (1 === res.status) { // 没有数据错误
                     typeof param.error === 'function' && param.error(res.msg);
                 }
             },
-            error: function(err) {
+            error: function (err) {
                 typeof param.error === 'function' && param.error(err.statusText);
             }
         });
     },
     // 获取服务器地址
-    getServerUrl: function(path) {
+    getServerUrl: function (path) {
         return conf.serverHost + path;
     },
 
     // 获取url参数
-    getUrlParam: function(name) {
+    getUrlParam: function (name) {
         var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)');
         var result = window.location.search.substr(1).match(reg);
-        return result ? encodeURIComponent(result[2]) : null;
+        return result ? decodeURIComponent(result[2]) : null;
     },
     //渲染html模板
-    renderHtml: function(htmlTemplate, data) {
+    renderHtml: function (htmlTemplate, data) {
         var template = Hogan.compile(htmlTemplate),
             result = template.render(data);
         return result;
     },
     // 成功提示
-    successTips: function(msg) {
+    successTips: function (msg) {
         alert(msg || '操作成功!');
     },
     // 错误提示
-    errorTips: function(msg) {
+    errorTips: function (msg) {
         alert(msg || '哪里不对了!');
     },
     // 字段的验证，支持非空判断，手机，邮箱
-    validate: function(value, type) {
+    validate: function (value, type) {
         var value = $.trim(value);
         // 非空验证
         if ('require' === type) {
@@ -68,13 +68,13 @@ var _mm = {
         if ('email' === type) {
             return /^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$/.test(value);
         }
-        
+
     },
     // 统一登录处理
-    doLogin: function() {
+    doLogin: function () {
         window.location.href = './user-login.html?redirect=' + encodeURIComponent(window.location.href);
     },
-    goHome: function() {
+    goHome: function () {
         window.location.href = './index.html';
     }
 };
