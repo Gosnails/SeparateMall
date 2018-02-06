04bedbceebb2c58ecc711d83cd781929c29a755c
Songzhiguo <Fu_shide@163.com>
2018-02-06 19:03:28 +0800

    订单确认页完成

---
+++ src/service/order-service.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var _mm = require('util/mm.js');
+
+var _order = {
+
+    // 商品列表数据
+    getProductList: function (resolve, reject) {
+        _mm.request({
+            url: _mm.getServerUrl('/order/get_order_cart_product.do'),
+            success: resolve,
+            error: reject
+        });
+    },
+    // 提交订单
+    createOrder: function (orderInfo, resolve, reject) {
+        _mm.request({
+            url: _mm.getServerUrl('/order/create.do'),
+            data: orderInfo,
+            success: resolve,
+            error: reject
+        });
+    },
+};
+module.exports = _order;
